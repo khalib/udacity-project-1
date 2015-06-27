34ec628ca185b5c9958de30532ccd50aa41296d8
khalib <caleb.strumms@gmail.com>
2015-06-27 03:02:28 -0700

    Initial commit. Artist search page and artist details page laid out.

---
+++ app/src/main/java/com/calebwhang/spotifystreamer/Artist.java
@@ -0,0 +1,16 @@
+package com.calebwhang.spotifystreamer;
+
+/**
+ * Created by caleb on 6/13/15.
+ */
+public class Artist {
+    String name;
+
+    public Artist(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ app/src/main/java/com/calebwhang/spotifystreamer/TrackListActivity.java
@@ -0,0 +1,7 @@
+package com.calebwhang.spotifystreamer;
+
+/**
+ * Created by caleb on 6/27/15.
+ */
+public class TrackListActivity {
+}
