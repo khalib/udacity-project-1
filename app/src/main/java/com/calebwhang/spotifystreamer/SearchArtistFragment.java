20d955e2f73c9b972520802f58251ba75f5aedcd
khalib <caleb.strumms@gmail.com>
2015-07-21 06:39:48 -0700

    Modified the search input from EditText to SearchView

--- app/src/main/java/com/calebwhang/spotifystreamer/SearchArtistFragment.java
+++ app/src/main/java/com/calebwhang/spotifystreamer/SearchArtistFragment.java
@@ -3,14 +3,12 @@ package com.calebwhang.spotifystreamer;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -21,6 +19,7 @@ import java.util.ArrayList;
 public class SearchArtistFragment extends Fragment {
 
     private final String LOG_TAG = SearchArtistFragment.class.getSimpleName();
+
     private static final String SELECTED_KEY = "selected_position";
     private static final String ARTIST_SEARCH_RESULTS_KEY = "artist_search_results";
     private static final String ARTIST_SEARCH_TEXT_KEY = "artist_search_text";
@@ -30,7 +29,6 @@ public class SearchArtistFragment extends Fragment {
     private ListView mListView;
     private SearchArtistAdapter mSearchArtistAdapter;
     private ArrayList<ArtistParcelable> mArtistSearchResults;
-    private int mPosition = ListView.INVALID_POSITION;
 
     public SearchArtistFragment() {
     }
@@ -43,7 +41,6 @@ public class SearchArtistFragment extends Fragment {
             // Load the stored states.
             mSearchText = savedInstanceState.getString(ARTIST_SEARCH_TEXT_KEY);
             mArtistSearchResults = savedInstanceState.getParcelableArrayList(ARTIST_SEARCH_RESULTS_KEY);
-            mPosition = savedInstanceState.getInt(SELECTED_KEY);
         } else {
             mSearchText = "";
             mArtistSearchResults = new ArrayList<ArtistParcelable>();
@@ -70,9 +67,29 @@ public class SearchArtistFragment extends Fragment {
 
                     startActivity(intent);
                 }
+            }
+        });
 
-                mPosition = position;
-                Log.v(LOG_TAG, Integer.toString(mPosition));
+        // Initialize search view.
+        SearchView searchText = (SearchView) mRootView.findViewById(R.id.search_text);
+        searchText.setIconifiedByDefault(false);
+        searchText.setQueryHint(getResources().getString(R.string.artist_search_hint));
+        searchText.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                // Search is already performed on text change.
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                // Assign the search string and perform the search.
+                if (!mSearchText.equals(s.toString())) {
+                    mSearchText = s.toString();
+                    getArtistSearchResults();
+                }
+
+                return false;
             }
         });
 
@@ -80,48 +97,10 @@ public class SearchArtistFragment extends Fragment {
     }
 
     @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        // Set a listener for when a search query is being typed in.
-        EditText searchEditText = (EditText) mRootView.findViewById(R.id.search_text);
-        searchEditText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                // Assign the search string and perform the search.
-                if (!mSearchText.equals(s.toString())) {
-                    mSearchText = s.toString();
-                    getArtistSearchResults();
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-
-        // Restore scroll position.
-        if (mPosition != ListView.INVALID_POSITION) {
-            mListView.smoothScrollToPosition(mPosition);
-        }
-
-        super.onActivityCreated(savedInstanceState);
-    }
-
-    @Override
     public void onSaveInstanceState(Bundle outState) {
         // Store activity states.
         outState.putString(ARTIST_SEARCH_TEXT_KEY, mSearchText);
         outState.putParcelableArrayList(ARTIST_SEARCH_RESULTS_KEY, mArtistSearchResults);
-
-        // Store the scroll position.
-        if (mPosition != ListView.INVALID_POSITION) {
-            outState.putInt(SELECTED_KEY, mPosition);
-        }
 
         super.onSaveInstanceState(outState);
     }
